dcbc627591321103987b0449026a2c37b1dc5df5
pkatanic <p.katanic@yahoo.com>
2020-12-30 03:34:21 +0300

    Chat window program init

---
+++ JavaLevel2HW4/src/module-info.java
@@ -0,0 +1,6 @@
+module JavaLevel2HW4 {
+    requires javafx.fxml;
+    requires javafx.controls;
+    opens sample;
+
+}
---
+++ JavaLevel2HW4/src/sample/Controller.java
@@ -0,0 +1,22 @@
+package sample;
+
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.TextArea;
+import javafx.scene.control.TextField;
+import javafx.scene.input.KeyEvent;
+
+public class Controller {
+    @FXML
+    private TextArea textArea;
+
+    @FXML
+    private TextField txtWrite;
+
+
+    public void msgSend(ActionEvent actionEvent) {
+    }
+
+    public void keyListener(KeyEvent keyEvent) {
+    }
+}
